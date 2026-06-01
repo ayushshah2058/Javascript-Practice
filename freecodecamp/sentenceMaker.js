8b6c617ecd4e71bab258de64fa149615b7686bc7
Ayush shah <120931510+ayushshah2058@users.noreply.github.com>
2026-06-01 10:49:22 +0000

    Build Sentence Maker by freecodecamp

---
+++ freecodecamp/sentenceMaker.js
@@ -0,0 +1,18 @@
+let adjective = "new",
+noun = "Ram",
+verb = "play",
+place = "kathmandu",
+adjective2 = "delicious",
+noun2 ="rice";
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective +" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+firstStory);
+adjective = "old",
+noun = "sita",
+verb = "sings",
+place = "janakpur",
+adjective2 = "amazing",
+noun2 ="guitar";
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective +" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+secondStory);
